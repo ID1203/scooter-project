5f52d2a1eb9bdf17a0ff07da08332239c7ca3209
Isaac Dunbar <i_dunbar@mail.com>
2024-10-25 13:39:55 +0100

    Completed scooter class

--- __tests__/Scooter.js
+++ __tests__/Scooter.js
@@ -2,32 +2,51 @@ const { describe, expect, it } = require("@jest/globals");
 const Scooter = require("../classes/Scooter.js");
 
 describe("scooter.rent(user)", () => {
-  it.skip("checks a scooter out to a user", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("checks a scooter out to a user", () => {
+    const Scooter1 = new Scooter("Main Station");
+    const user = { username: "Isaac" };
+    Scooter1.rent(user)
+    expect(Scooter1.user).toBe(user)
+
   });
 
-  it.skip("throws an error if battery dead or scooter broken", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if battery dead", () => {
+    const Scooter1 = new Scooter("blueNorth")
+    const user1 = { username: "Isaac"};
+    Scooter1.charge = 19
+    expect(() => Scooter1.rent(user1)).toThrow("scooter needs to charge");
   });
+
+  it("throws an error if scooter broken", () => {
+    const Scooter1 = new Scooter("blueNorth")
+    const user1 = { username: "Isaac"};
+    Scooter1.isBroken = true
+    expect(() => Scooter1.rent(user1)).toThrow("scooter needs repair");
+  });
+
+
 });
 
 describe("scooter.dock(station)", () => {
-  it.skip("returns a scooter to a station", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("returns a scooter to a station", () => {
+    const Scooter1 = new Scooter("blueNorth")
+    const user1 = { username: "Isaac"};
+    Scooter1.rent(user1)
+    Scooter1.dock("redWest")
+    expect(Scooter1.station).toBe("redWest")
+    expect(Scooter1.user).toBe(null)
+
   });
 });
 
 describe("scooter.charge()", () => {
-  it.skip("charges a scooter", () => {
-    // Arrange
-    // Act
-    // Assert
+  it.skip("charges a scooter", async () => {
+    const Scooter1 = new Scooter("blueNorth")
+    Scooter1.charge = 50
+    Scooter1.reCharge()
+
+    expect(Scooter.charge).toBe(100)
+
   });
 });
 
--- classes/Scooter.js
+++ classes/Scooter.js
@@ -1,5 +1,63 @@
 class Scooter {
-  // Scooter code here
+  static nextSerial = 1
+  constructor(station){
+    this.station = station
+    this.user = null
+    this.serial = Scooter.nextSerial++
+    this.charge = 100
+    this.isBroken = false
+  }
+  rent(user){
+    if(this.charge > 20 && this.isBroken == false){
+      this.user = user
+    }
+    else if(this.charge > 20 && this.isBroken == true){
+      throw new Error("scooter needs repair")
+    }
+    else{
+      throw new Error("scooter needs to charge")
+    }
+  }
+  dock(station){
+    this.station = station
+    this.user = null
+  }
+  reCharge() {
+    const rechargeTime = setInterval(() => {
+        if (this.charge < 100) {
+            this.charge += 10; 
+            console.log(`Battery: ${this.charge}%`);
+        } else {
+            clearInterval(rechargeTime); 
+            console.log("Scooter is fully charged!");
+        }
+    }, 1000);
+  }
+  repair(){
+    const repair = setInterval(() => {
+      if(this.isBroken == true){
+        this.isBroken = false
+        console.log("repair completed")
+        clearInterval(repair); 
+      }
+    }, 5000)
+  }
 }
+
+
+// function debug(){
+//   const Scooter1 = new Scooter("blueNorth")
+//   Scooter1.charge = 30
+//   Scooter1.isBroken = true
+//   //Scooter1.reCharge()
+//   //Scooter1.dock("redWest")
+//   const user1 = { username: "Isaac", password: "password123", age: 22 };
+//   //Scooter1.rent(user1)
+//   Scooter1.repair()
+
+//   console.log(Scooter1)
+// }
+
+// debug()
 
 module.exports = Scooter;
