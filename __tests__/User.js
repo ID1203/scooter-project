8639f589d2f0a20d0f465c456de3da02ff92c9ef
Isaac Dunbar <i_dunbar@mail.com>
2024-10-25 14:15:22 +0100

    Completed user class

--- __tests__/User.js
+++ __tests__/User.js
@@ -2,23 +2,23 @@ const { describe, expect, it } = require("@jest/globals");
 const User = require("../classes/User.js");
 
 describe("user.login(password)", () => {
-  it.skip("logs a user in if the password is correct", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs a user in if the password is correct", () => {
+    const user = new User("Isaac", "password123", 22)
+    user.login("password123")
+    expect(user.loggedIn).toBe(true)
   });
 
-  it.skip("throws an error if the password is incorrect", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("throws an error if the password is incorrect", () => {
+    const user = new User("Isaac", "password123", 22)
+    expect(() => user.login("wrongpassword")).toThrow("incorrect password")
   });
 });
 
 describe("user.logout()", () => {
-  it.skip("logs a user out", () => {
-    // Arrange
-    // Act
-    // Assert
+  it("logs a user out", () => {
+    const user = new User("Isaac", "password123", 22)
+    user.loggedIn = true; 
+    user.logout();
+    expect(user.loggedIn).toBe(false); 
   });
 });
